bf2fdf58cdd1004de7c1bf95d85945c980e0f00a
j0sa <jonas.arucard@gmail.com>
2020-09-21 11:15:49 +0200

    added slideshow and js

---
+++ scripts/imageSlideshow.js
@@ -0,0 +1,60 @@
+/* var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+} */
+
+let slideIndex = 1;
+
+automaticSlideshow();
+displayImages(slideIndex);
+
+function changeSlideIndex(n) {
+    displayImages(slideIndex += n);
+}
+
+function displayImages(n) {
+    var i;
+    var x = document.getElementsByClassName("slidesshowimage");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
+
+function automaticSlideshow() {
+    var i;
+    var x = document.getElementsByClassName("slidesshowimage");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(automaticSlideshow, 10000);
+}
