5bd516a35580f946b5f8f71cf3b0b187b3d4fcc3
j0sa <jonas.arucard@gmail.com>
2020-09-23 15:28:24 +0200

    animations to resume

---
+++ scripts/individual-scripts/jonasSamuelsson.js
@@ -0,0 +1,30 @@
+function move() {
+    var elem = document.getElementById("progress-bar");
+    var width = 0.1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
+
+function moveTwo() {
+    var elem = document.getElementById("progress-bar");
+    var width = 20;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            elem.innerHTML = width * 1 + '%';
+        }
+    }
+}
+
+window.onload(moveTwo());
