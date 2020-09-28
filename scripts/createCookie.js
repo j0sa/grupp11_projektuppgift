f9e58f1e48a2fcea850e3abf0f8f573e70f98336
j0sa <jonas.arucard@gmail.com>
2020-09-28 12:51:51 +0200

    cleanup

--- scripts/createCookie.js
+++ scripts/createCookie.js
@@ -1,9 +1,7 @@
 function redirectIfCookieFound() {
-    let COOKIE_NAME = Cookies.get('firstvisit');
+    let COOKIE_NAME = Cookies.get('visited');
     if (COOKIE_NAME == undefined) {
-        Cookies.set('firstvisit', 'true', { expires: 10 });
+        Cookies.set('visited', 'true', { expires: 10 });
         window.location = '/start.html';
-    } else {
-        window.location = '/index.html';
     }
 }
