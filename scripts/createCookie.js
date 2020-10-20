09f0fbb5d2e30e63b2d9f88d82ca0af16da955db
Jonas Samuelsson <josa@Jonass-MacBook-Pro.local>
2020-10-20 18:33:41 +0200

    asdasd

--- scripts/createCookie.js
+++ scripts/createCookie.js
@@ -2,6 +2,6 @@ function redirectIfCookieFound() {
     let COOKIE_NAME = Cookies.get('visited');
     if (COOKIE_NAME == undefined) {
         Cookies.set('visited', 'true', { expires: 10 });
-        window.location = '/start.html';
+        window.location = 'start.html';
     }
 }
